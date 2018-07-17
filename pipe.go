1bce8019425eaf712dc30ba8bb1654c61e866cc0
rh01 <1048157315@qq.com>
2018-07-17 19:36:09 +0800

    final version

--- pipe.go
+++ pipe.go
@@ -68,11 +68,17 @@ func (ps *pipes) update() {
 	ps.mu.Lock()
 	defer ps.mu.Unlock()
 
+	var rem []*pipe
 	for _, p := range ps.pipes {
 		p.mu.Lock()
 		p.x -= ps.speed
 		p.mu.Unlock()
+		if p.x > 0 {
+			rem = append(rem, p)
+		}
 	}
+
+	ps.pipes = rem
 
 }
 
